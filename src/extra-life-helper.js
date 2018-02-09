5805faa874674bd577c12cb965e76b13188788ca
jstudz <jstudzinski1@my.wctc.edu>
2018-02-08 19:43:19 -0600

    Added boostrap module

--- src/extra-life-helper.js
+++ src/extra-life-helper.js
@@ -1,15 +1,24 @@
 const elAPI = require('extra-life-api');
 var participantId = "296732";
-var num = 1;
-
-var intervalId = setInterval(pullData, 60000);
+var totalRaisedAmount = 0;
+var fundraisingGoal = 0;
 
 function pullData()
 {
     elAPI.getUserInfo(participantId, false).then(data => {
         console.log(data);
-        document.getElementById("totalAmount").innerText = "$" + data.totalRaisedAmount + "/$" + data.fundraisingGoal;
+        document.getElementById("totalAmount").innerText = "Extra Life Goal $" + data.totalRaisedAmount + "/$" + data.fundraisingGoal;
+        //setProgressValue(data.totalRaisedAmount, data.fundraisingGoal);
     });
+
+    var timeoutId = setTimeout(pullData, 60000);
 }
 
 window.onload = pullData;
+
+function setProgressValue(totalRaisedAmount, fundraisingGoal)
+{
+    var percentage =  Math.round((totalRaisedAmount /fundraisingGoal) * 100, -1);
+    document.getElementById("progress").setAttribute("aria-valuenow", percentage);
+    document.getElementById("progress").setAttribute("style", "width:" + percentage);
+}
