9cced3533da3e7494ebe98c8b9fda056b6636085
jstudz <jstudzinski1@my.wctc.edu>
2018-02-10 22:45:13 -0600

    Added el-logo, styled it, fixed error in javascript and added userData.json

--- src/extra-life-helper.js
+++ src/extra-life-helper.js
@@ -1,23 +1,25 @@
 const elAPI = require('extra-life-api');
-var participantId = "296732";
-var userInfo = "https://www.extra-life.org/index.cfm?fuseaction=donorDrive.participant&participantID=" +participantId +"&format=json";
+const userData = require('../userData.json')
+var PARTICIPANT_URL = "https://www.extra-life.org/index.cfm?fuseaction=donorDrive.participant&participantID={participantId}&format=json"
 
-window.onload = getJson();
+window.onload = getExtraLifeData();
 
-function setProgressValue(totalRaisedAmount, fundraisingGoal)
+function getExtraLifeData()
 {
-    var percentage =  Math.round((totalRaisedAmount /fundraisingGoal) * 100, -1);
-    document.getElementById("progress").setAttribute("aria-valuenow", percentage);
-    document.getElementById("progress").setAttribute("style", "width:" + percentage);
-}
-
-function getJson()
-{
-    $.getJSON(userInfo, function(data) {
+    $.getJSON(PARTICIPANT_URL.replace("{participantId}", userData.participantId), function(data) {
         console.log(data);
-        document.getElementById("totalAmount").innerText = "Extra Life Goal $" + data.totalRaisedAmount + "/$" + data.fundraisingGoal;
+        var goal = userData.goal != 0 ? userData.goal : data.fundraisingGoal;
+        document.getElementById("totalAmount").innerText = "Extra Life Goal $" + data.totalRaisedAmount + "/$" + goal;
+        //setProgressBar(data.totalRaisedAmount, goal);
     });
 
-    var timeoutId = setTimeout(pullData, 60000);
+    var timeoutId = setTimeout(getExtraLifeData, 60000);
 }
+
+function setProgressBar(totalRaised, goal)
+{
+    var progress = (totalRaised / goal) * 100;
+    $('#progress-bar').attr('aria-valuenow', progress).css('width', progress + "%").text("Extra Life Goal $" + totalRaised + "/$" + goal);
+}
+
 
